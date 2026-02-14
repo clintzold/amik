b941b9f058ec383beadc8dbb4ec0290d1b0941e7
Clint Zold <clintzold@users.noreply.github.com>
2026-02-14 01:40:44 -0700

    setup shallow nested routes for Posts and Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :posts do
+    resources :comments, shallow: true
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +16,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
 end
