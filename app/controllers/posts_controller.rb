c31bee117e1139867ff70473cad582c2ac7495c7
clint zold <214383878+clintzold@users.noreply.github.com>
2026-02-17 21:09:17 -0700

    display images in posts, continue styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:body, :images)
+    params.require(:post).permit(:body, images: [])
   end
 
   def set_post
